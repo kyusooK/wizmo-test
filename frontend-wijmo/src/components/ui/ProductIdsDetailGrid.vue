3fc906601a78bb3e8d56e072440a81c107557c21
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2023-08-11 13:38:12 +0900

    commit

---
+++ frontend-wijmo/src/components/ui/ProductIdsDetailGrid.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <wj-flex-grid
+            ref="flexGridDetails"
+            :key="value.length"
+            :autoGenerateColumns="false"
+            :allowAddNew="false"
+            :allowDelete="true"
+            :allowPinning="'SingleColumn'"
+            :newRowAtTop="false"
+            :showMarquee="true"
+            :selectionMode="'MultiRange'"
+            :validateEdits="false"
+            :itemsSource="value"
+            :initialized="flexDetailsInitialized"
+            style="margin-top:10px; max-height:65vh;"
+        >
+            <wj-flex-grid-cell-template cellType="RowHeader" v-slot="cell">{{cell.row.index + 1}}</wj-flex-grid-cell-template>
+            
+            <wj-flex-grid-column binding="name" header="제품ID" width="2*" :isReadOnly="true" align="center">
+                <wj-flex-grid-cell-template cellType="Cell" v-slot="cell">
+                    <String label="입력하세요." v-model="cell.item.name" :editMode="editMode"/>
+                </wj-flex-grid-cell-template>
+            </wj-flex-grid-column>
+        </wj-flex-grid>
+        
+        <v-btn v-if="editMode" @click="append()">추가</v-btn>
+        <v-btn v-if="editMode" @click="detailDeleteRow()">삭제</v-btn>
+    </div>
+</template>
+
+<script>
+import BaseDetailGrid from '../base-ui/BaseDetailGrid';
+import ProductId from '../ProductId.vue';
+
+export default {
+    name: 'ProductIdsDetailGrid',
+    mixins: [BaseDetailGrid],
+    components: {
+        ProductId
+    },
+    methods: {
+    }
+}
+</script>
+
---
+++ sales/src/main/java/wizmokeycloak/infra/SalesOrderHateoasProcessor.java
@@ -0,0 +1,23 @@
+package wizmokeycloak.infra;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelProcessor;
+import org.springframework.stereotype.Component;
+import wizmokeycloak.domain.*;
+
+@Component
+public class SalesOrderHateoasProcessor
+    implements RepresentationModelProcessor<EntityModel<SalesOrder>> {
+
+    @Override
+    public EntityModel<SalesOrder> process(EntityModel<SalesOrder> model) {
+        model.add(
+            Link
+                .of(model.getRequiredLink("self").getHref() + "/produce")
+                .withRel("produce")
+        );
+
+        return model;
+    }
+}
